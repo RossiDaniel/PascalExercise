fbada13d21a0b8f1cc60548225d7e9edaa6f601f
RossiDaniel <bovo.96@gmail.com>
2019-01-26 17:14:27 +0100

    quadrato di cesare, pattern matching con stringhe

--- C++/es12.cpp
+++ C++/es12.cpp
@@ -9,7 +9,6 @@ int is_perfect_square(int n) {
     if (n < 0)
         return 0;
     int root(round(sqrt(n)));
-    cout<<root;
     if (n == root * root){
         return root;
     }
@@ -26,20 +25,22 @@ int main(){
     gets(frase);
     //strlen restituisce la lunghezza della stringa
     int len=strlen(frase);
-    cout<<len;
     int root=is_perfect_square(len);
-    cout<<root;
     if(root){
         for(int i=0; i< root; i++){
             for(int j=0; j<root;j++){
                 str[i][j]=frase[root*j+i];
             }
         }
-        for(int j=0; j< root; j++){
-            for(int i=0; i<root;i++){
-                std::cout<<str[i][j];
+
+        for(int i=0; i< root; i++){
+            for(int j=0; j<root;j++){
+                        std::cout<<str[i][j];
+                frase[i*root+j]=str[i][j]+2;
             }
         }
+        std::cout<<std::endl;
+        std::cout<<frase;
     }
     else{
         std::cout<<"non è un quadrato perfettto"<<std::endl;
